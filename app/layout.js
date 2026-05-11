0236199fab32266c5698b662a7edafc352f081aa
Ahmed_khalil <ahgadoora2002@gmail.com>
2026-05-11 14:24:51 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="ar">
+    <html lang="ar" dir="rtl">
       <body className={`${cairo.className} bg-white text-gray-900`}>
         <Navbar />
         {children}
