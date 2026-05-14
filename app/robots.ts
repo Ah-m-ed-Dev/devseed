11c95b21e87d50f4a7e17773caf17d604417af1f
Ahmed_khalil <ahgadoora2002@gmail.com>
2026-05-14 13:56:50 +0200

    Create robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl =
+    process.env.NEXT_PUBLIC_SITE_URL ||
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL || "devseed.vercel.app"}`;
+
+  return {
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api/", "/_next/"],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
+}
