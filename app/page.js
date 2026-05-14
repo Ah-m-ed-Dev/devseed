d8d9ab8bfa129ee8a919e3daf76adb1d77b2830f
Ahmed_khalil <ahgadoora2002@gmail.com>
2026-05-14 13:59:31 +0200

    Update page.js add metadata

--- app/page.js
+++ app/page.js
@@ -4,9 +4,19 @@ import Projects from "@/sections/projects";
 import About from "@/sections/about";
 import Contact from "@/sections/contact";
 
+export const metadata = {
+  title: "DevSeed | نزرع حلولاً رقمية",
+  description:
+    "حوّل فكرتك إلى منتج رقمي قابل للتوسع. DevSeed تبني مواقع وتطبيقات حديثة باستخدام أحدث التقنيات.",
+  alternates: {
+    canonical: "/",
+  },
+};
+
 export default function Home() {
   return (
     <main className="overflow-hidden">
+      <Navbar />
       <Hero />
       <Services />
       <Projects />
