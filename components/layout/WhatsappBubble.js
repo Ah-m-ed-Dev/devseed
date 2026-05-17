4e8900fc7952486130498ec0efde0c3998edce71
Ahmed_khalil <ahgadoora2002@gmail.com>
2026-05-17 14:32:09 +0200

    Update WhatsappBubble.js

--- components/layout/WhatsappBubble.js
+++ components/layout/WhatsappBubble.js
@@ -80,11 +80,11 @@ export default function WhatsappBubble() {
               }}
             >
               ابدأ المحادثة
-              {/* سهم تيليجرام */}
-              <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M5 12h14" />
-                <path d="M12 5l7 7-7 7" />
-              </svg>
+              {/* سهم ↗️ احترافي */}
+<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round">
+  <path d="M7 17L17 7" />
+  <path d="M7 7h10v10" />
+</svg>
             </a>
           </div>
         </div>
