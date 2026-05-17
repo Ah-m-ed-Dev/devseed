d48f53a8cdba02984e92acfbb959ecbbc0482d6b
Ahmed_khalil <ahgadoora2002@gmail.com>
2026-05-17 11:18:10 +0200

    Update contact.js

--- sections/contact.js
+++ sections/contact.js
@@ -4,7 +4,8 @@ export default function Contact() {
       
       {/* خلفية زخرفية */}
       <div className="absolute inset-0 bg-[linear-gradient(rgba(20,184,166,0.02)_1px,transparent_1px),linear-gradient(90deg,rgba(20,184,166,0.02)_1px,transparent_1px)] bg-[size:80px_80px]" />
-      <div className="absolute top-0 left-1/2 -translate-x-1/2 w-[600px] h-[400px] bg-teal-500/5 rounded-full blur-[120px]" />
+      <{/* وهج مركزي */}
+      {<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] sm:w-[600px] h-[300px] sm:h-[600px] bg-teal-500/5 rounded-full blur-[120px]" />
 
       <div className="max-w-4xl mx-auto relative z-10">
         
