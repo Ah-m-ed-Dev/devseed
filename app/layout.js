bad5c8a9f16bffdb104bb22cd393f8d24da61fa6
Ahmed_khalil <ahgadoora2002@gmail.com>
2026-05-17 10:23:34 +0200

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
 import { Cairo } from "next/font/google";
-import WhatsappBubble from "@/components/WhatsappBubble";
+import WhatsappBubble from "@/components/layout/WhatsappBubble";
 
 const cairo = Cairo({
   subsets: ["arabic"],
