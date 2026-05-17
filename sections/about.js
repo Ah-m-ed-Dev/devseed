8e025045615b9366a2288fd869b3bb940caa5141
Ahmed_khalil <ahgadoora2002@gmail.com>
2026-05-17 11:16:15 +0200

    Update about.js

--- sections/about.js
+++ sections/about.js
@@ -12,7 +12,7 @@ export default function About() {
       <div className="absolute inset-0 bg-[linear-gradient(rgba(20,184,166,0.02)_1px,transparent_1px),linear-gradient(90deg,rgba(20,184,166,0.02)_1px,transparent_1px)] bg-[size:80px_80px]" />
       
       {/* وهج مركزي */}
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-teal-500/5 rounded-full blur-[120px]" />
+      {<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[300px] sm:w-[600px] h-[300px] sm:h-[600px] bg-teal-500/5 rounded-full blur-[120px]" />
 
       <div className="max-w-5xl mx-auto relative z-10">
         
