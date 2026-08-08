dda397c501a6b130c5aa5d6a0a7cd0d26a8e782f
Ahmed_khalil <ahgadoora2002@gmail.com>
2026-08-08 14:30:31 +0200

    Update layout.js add aibot

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import "./globals.css";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
 import { Cairo } from "next/font/google";
-import WhatsappBubble from "@/components/layout/WhatsappBubble";
+import DevSeedChatBot from '@/components/layout/DevSeedChatBot';
 
 const cairo = Cairo({
   subsets: ["arabic"],
@@ -89,7 +89,8 @@ export default function RootLayout({ children }) {
         <Navbar />
         <main>{children}</main>
         <Footer />
-        <WhatsappBubble />
+        <DevSeedChatBot />
+        
       </body>
     </html>
   );
